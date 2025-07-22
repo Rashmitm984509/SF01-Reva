5af9a34d97479f9f519e9e039e8f6f9b6dc0b0ca
TNS INDIA <TNS INDIA@LAPTOP-N0JHV8G0>
2025-07-22 15:19:03 +0530

    first commit

---
+++ Corejavaprograms/src/com/tnsif/praticedemo/Pratice.java
@@ -0,0 +1,15 @@
+package com.tnsif.praticedemo;
+
+public class Pratice {
+ int a=8;
+ String s="rashmi";   // properties 
+ 
+ 
+ void display() {
+	 System.out.println("welcome to java");// behavior
+ }
+ 
+ public static void main(String[] args) { // main method
+	
+}
+}
